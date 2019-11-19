e24063e1fc7ef9ef4ba3f2686c7868e80fb9febb
tosvt <sweb7@ya.ru>
2019-11-19 19:34:30 +0300

    New code changes

--- app/src/main/java/com/example/authapppractice/Auth.java
+++ app/src/main/java/com/example/authapppractice/Auth.java
@@ -2,13 +2,21 @@ package com.example.authapppractice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Auth extends AppCompatActivity {
-
+    TextView successEnter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
+
+        successEnter = findViewById(R.id.succussEnter);
+        Intent intent = getIntent();
+        //String name = intent.getStringExtra("login");
+
+        successEnter.setText("Вы успешно авторизировались!");
     }
 }
--- app/src/main/java/com/example/authapppractice/MainActivity.java
+++ app/src/main/java/com/example/authapppractice/MainActivity.java
@@ -7,30 +7,40 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import java.sql.SQLOutput;
 
 public class MainActivity extends AppCompatActivity {
+    EditText editLogin;
+    EditText editPassword;
+    Button btn_auth;
+    TextView textinfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editLogin = findViewById(R.id.editLogin);
+        editPassword = findViewById(R.id.editPassword);
+        textinfo = findViewById(R.id.textinfo);
+        btn_auth = findViewById(R.id.btn_auth);
+        // btn_auth.setOnClickListener(thi);
     }
 
     public void onClick(View v){
+        Intent intent = new Intent(this, Auth.class);
+        intent.putExtra("login", editLogin.getText().toString());
+        intent.putExtra("pass", editPassword.getText().toString());
+        String login = intent.getStringExtra("login");
+        String pass = intent.getStringExtra("pass");
 
-        switch(v.getId()){
-            case R.id.btn_auth:
-
-                Intent other = new Intent(this, Auth.class);
-                EditText ed = findViewById(R.id.editLogin);
-                other.putExtra("Login", ed.getText().toString());
-                startActivity(other);
-                break;
-
-
-                break;
-        }
-
+        if(login.equals("Svyat") && pass.equals("pass123"))
+        {
+        startActivity(intent);}
+        else textinfo.setText("Неправильная пара логин и пароль!");
     }
 }
